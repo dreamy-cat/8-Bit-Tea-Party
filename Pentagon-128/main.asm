8592212fc6c2c7399bd70d80c5fec3f78b27285c
Alexander Serov <alexander.serov@protonmail.com>
2019-09-07 20:51:10 +0300

    Set pixel ready.

--- Pentagon-128/main.asm
+++ Pentagon-128/main.asm
@@ -1,1 +1,108 @@
-        ORG #6000;TESTING COMMENT 866 CODE PAGE.;Тестирование кодировки 866.        LD DE,#AA25        CALL CLEARSCREEN        RET ;CLEAR SCREEN FUNCTION AND TESTING.;D - BYTE FOR FILL SCREEN.;E - BYTE FOR FILL ATTRIBUTES.;Тестирование функции заполнения.CLEARSCREEN:        PUSH AF        PUSH BC        PUSH DE        PUSH HL        LD HL,#4000        LD BC,#1800CLR_1:  LD A,D        LD (HL),A        INC HL        DEC BC        LD A,B        OR C        JR NZ,CLR_1        LD BC,#300CLR_2:  LD A,E        LD (HL),E        INC HL        DEC BC        LD A,B        OR C        JR NZ,CLR_2        POP HL        POP DE        POP BC        POP AF        RET 
+        ORG #6000
+
+;Глобальные имена и переменные.
+
+SCREEN_RAM      EQU #4000
+SCREEN_SIZE     EQU #1800
+SCREEN_ATTRIB   EQU #5800
+ATTRIB_SIZE     EQU #300
+
+        LD D,#00
+        LD E,%01000111
+        CALL CLEARSCREEN
+        LD D,2
+        LD E,190
+        CALL SET_PIXEL
+        RET 
+
+;SET PIXEL ON SCREEN, WITH COORDINATES.
+;REGISTERS:
+;D - X (0..255), E - Y (0..191)
+;RETURN: ON SCREEN.
+
+SET_PIXEL:
+        PUSH AF
+        PUSH BC
+        PUSH DE
+        PUSH HL
+        LD HL,SCREEN_RAM
+        LD A,E          ;2048 BYTES PART.
+        AND %11000000
+        RRCA 
+        RRCA 
+        RRCA 
+        LD B,A
+        LD A,H
+        OR B
+        LD H,A
+        LD A,E          ;256 BYTES PART.
+        AND %00000111
+        LD B,A
+        LD A,H
+        OR B
+        LD H,A
+        LD A,E          ;32 BYTES PART.
+        AND %00111000
+        RLCA 
+        RLCA 
+        LD B,A
+        LD A,L
+        OR B
+        LD L,A
+        LD A,D          ;HORIZONTAL BYTE.
+        AND %11111000
+        RRCA 
+        RRCA 
+        RRCA 
+        LD B,A
+        LD A,L
+        OR B
+        LD L,A
+        LD A,D          ;PIXEL IN BYTE.
+        AND %00000111
+        LD B,A
+        LD A,%10000000
+PIX_1:  RRCA 
+        DJNZ PIX_1
+        OR (HL)
+        LD (HL),A
+        POP HL
+        POP DE
+        POP BC
+        POP AF
+        RET 
+
+
+;CLEAR SCREEN FUNCTION AND TESTING.
+;D - BYTE FOR FILL SCREEN.
+;E - BYTE FOR FILL ATTRIBUTES.
+;Тестирование функции заполнения.
+
+
+CLEARSCREEN:
+        PUSH AF
+        PUSH BC
+        PUSH DE
+        PUSH HL
+        LD HL,SCREEN_RAM
+        LD BC,#1800
+CLR_1:  LD A,D
+        LD (HL),A
+        INC HL
+        DEC BC
+        LD A,B
+        OR C
+        JR NZ,CLR_1
+        LD BC,#300
+CLR_2:  LD A,E
+        LD (HL),E
+        INC HL
+        DEC BC
+        LD A,B
+        OR C
+        JR NZ,CLR_2
+        POP HL
+        POP DE
+        POP BC
+        POP AF
+        RET 
